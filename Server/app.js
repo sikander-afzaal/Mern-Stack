80155d7da27db244ab907b1d2d98859770c4b5f8
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-07-09 19:15:06 +0500

    com

--- Server/app.js
+++ Server/app.js
@@ -8,7 +8,7 @@ dotenv.config({
   path: "./config.env",
 });
 
-const PORT = process.env.PORT; // port of the server
+const PORT = process.env.PORT || 5000; // port of the server
 
 //getting db connection code is in diff file so that we can use it everywhere with ease
 require("./db/conn");
@@ -16,6 +16,14 @@ require("./db/conn");
 app.use(express.json()); // converting all the data to json format
 app.use(cookieParser());
 app.use(require("./router/auth")); //we link the router file here so we can use the routes
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`port ${PORT}`);
